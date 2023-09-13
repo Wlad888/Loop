591c6016761692ab53d0bcbd115626d2ae353875
Wlad888 <vladesanu@gmail.com>
2023-09-13 16:22:21 +0300

    added to files

---
+++ Loop/src/ForLoop.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the digit ");
+        int n = scan.nextInt();
+        outer:
+        for (int i = 10; i > n; i--) {
+            System.out.println(i);
+            for (int j = 10; j > n; j--) {
+                System.out.println(j);
+                if (j == 8)
+                    break outer;
+            }
+        }
+    }
+}
